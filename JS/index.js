2f732e4a9776c69e0b32711f1c047ba91c07c82e
Sirius CMa <karpunaa@yandex.ru>
2023-01-21 22:25:18 +0300

    ***Доработки :*** *HTML* *JS* *CSS*

--- JS/index.js
+++ JS/index.js
@@ -9,15 +9,45 @@ function initSwitch() {
 
   lightButton.addEventListener('click', (evt) => {
     themes.forEach(elem => elem.classList.remove('dark'))
-    switchFlagBox.classList.remove('switch__flag-box_dark')
+    switchFlagBox.classList.remove('switch__flag-box_dark', 'move')
   })
 
   darkButton.addEventListener('click', (evt) => {
     themes.forEach(elem => elem.classList.add('dark'))
-    switchFlagBox.classList.add('switch__flag-box_dark')
+    switchFlagBox.classList.add('switch__flag-box_dark', 'move')
 
   })
 
 }
 
 initSwitch();
+
+function initMenuButton() {
+  const menu = document.querySelector('.menu')
+  const menuButton = menu.querySelector('.menu__button')
+  const switcher = menu.querySelector('.switch')
+  const subblock = menu.querySelector('.menu__subblock')
+
+  const openMenu = () => {
+    console.log('open');
+    subblock.classList.add('menu__subblock_open')
+  }
+
+  const closeMenu = () => {
+    console.log('close');
+    subblock.classList.remove('menu__subblock_open')
+  }
+
+  menuButton.addEventListener('click', (evt) => {
+    subblock.className.includes('menu__subblock_open') ?
+      closeMenu()
+      : openMenu()
+  })
+
+}
+
+
+
+
+
+initMenuButton();
